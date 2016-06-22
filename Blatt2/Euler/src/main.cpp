789d872c7ddef688a7b6f8024a6f97f2d44f2528
dln-dev <dln-dev@users.noreply.github.com>
2016-06-22 15:31:59 +0200

    bugfixes in Euler

--- Blatt2/Euler/src/main.cpp
+++ Blatt2/Euler/src/main.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <cmath>
 #include "../cmagic/cmagic.h"
+#include <typeinfo>
 
 using namespace std;
 
@@ -13,27 +14,33 @@ if(argc == 2) {
 	unsigned short int i;
 	unsigned short int n = 5;
 	unsigned short int j = 1;
+	unsigned short int precision = 0;
 
 	float quickE = 0;
 	double loopE = 1;
 	double mathE = exp(x);
 	float tolerance = 1.0E-6;
 
-	cout << x << endl;
-
-	cout.precision(12);
+	cout << "\nx:\t" << x << "\nType:\t" << typeid(x).name() << endl;
 
 	for(i = 0; i < n; i++) 
-		quickE += x / factorial(i);
+		quickE += pow(x,i) / factorial(i);
 
-	while(abs((loopE - mathE)/mathE) > tolerance) 
-		loopE += x / staticFac(j++);
+	while(abs((loopE - mathE)/mathE) > tolerance) {
+		loopE += pow(x, j) / staticFac(j);
+		j++;
+	}
+
+	//precision = round(1 - log10(abs(loopE - mathE)));
+	precision = 2 - log10(tolerance);
+
+	cout.precision(precision);
 
 	cout << "\ncmath:\t\t" << mathE << "\n";
 	cout << "---------------------------------\n";
 	cout << "5 step euler:\t" << quickE << "\n";
 	cout << "rel error:\t" << abs((quickE-mathE)/mathE) << "\n\n";
-	cout << "tolerance euler:" << loopE << "\n";
+	cout << "loop euler:\t" << loopE << "\n";
 	cout << "rel error:\t" << abs((loopE-mathE)/mathE) << "\n" << endl;
 }
 else {
