45cb3a7a80e6ee555b86d6b457eddd79116101b8
dln-dev <dln-dev@users.noreply.github.com>
2016-06-30 13:28:28 +0200

    add matrices from paper

--- Blatt2/Matrix/src/main.cpp
+++ Blatt2/Matrix/src/main.cpp
@@ -7,20 +7,48 @@ using namespace std;
 int main(void) {
 	unsigned short int i, j;
 	
-	float ** mat = dynMatrix(3, 3);
-	
+	/*float ** mat = dynMatrix(3, 3);
 	for(i = 0; i < 3; i++)
 		for(j = 0; j < 3; j++)
-			mat[i][j] = (i+1)*j;
+			mat[i][j] = (i+1)*j;*/
 
-	float b[][3] = {{1,2,3},{4,5,6},{7,8,9}};
-	float *b_dyn[3] = {b[0], b[1], b[2]};
+	float  A_stat[][2] = {{1,2},{3,0}};
+	float *A_init[2] = {A_stat[0], A_stat[1]};
+	float  B_stat[][3] = {{4,6,8},{11,2,12},{3,5,16}};
+	float *B_init[3] = {B_stat[0], B_stat[1], B_stat[2]};
+	float  H_stat[][3] = {{2,4,-2},{4,9,-3},{-2,-3,7}};
+	float *H_init[3] = {H_stat[0], H_stat[1], H_stat[2]};
+
+	float  b_stat[][1] = {{1},{3}};
+	float *b_init[2] = {b_stat[0], b_stat[1]};
+	float  u_stat[][3] = {4,6,8};
+	float *u_init[1] = {u_stat[0]};
+	float  v_stat[][1] = {{2},{8},{10}};
+	float *v_init[3] = {v_stat[0], v_stat[1], v_stat[2]};
+
+	CMatrix A = CMatrix(A_init, 2, 2);
+	CMatrix B = CMatrix(B_init, 3, 3);
+	CMatrix H = CMatrix(H_init, 3, 3);
+
+	CMatrix b = CMatrix(b_init, 2, 1);
+	CMatrix u = CMatrix(u_init, 1, 3);
+	CMatrix v = CMatrix(v_init, 3, 1);
+
+	A.print();
+	B.print();
+	H.print();
+
+	b.print();
+	u.print();
+	v.print();
+
+
 
 	/*cout << c[1][1] << endl;
 	cout << typeid(c).name() << endl;
 	cout << typeid(mat).name() << endl;*/
 	
-	CMatrix matrix = CMatrix(mat, 3, 3);
+	/*CMatrix matrix = CMatrix(mat, 3, 3);
 	CMatrix *mbtrix = new CMatrix(b_dyn, 3, 3);
 	CMatrix mctrix = CMatrix(matrix);
 	CMatrix mdtrix = CMatrix(nullptr, 3,3);
@@ -50,7 +78,7 @@ int main(void) {
 
 	delete mbtrix;
 	delete[] mat[0];
-	delete mat;
+	delete mat;*/
 
 	return 0;
 }
